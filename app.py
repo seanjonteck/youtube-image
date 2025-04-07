ef80fe626069db4ec23367fddf13b553cf654ba7
seanjonteck <seanjoner@gmail.com>
2025-04-07 20:57:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def serve_image(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
