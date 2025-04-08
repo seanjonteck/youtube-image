130bd7929098575e6a7566335ac53f82c6ad3d93
seanjonteck <seanjoner@gmail.com>
2025-04-08 17:13:36 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // express 모듈 불러오기
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // cors 모듈 추가
 
 // Express 앱 생성
 const app = express();
 
 // 포트 설정 (환경변수 PORT가 있으면 그 값을 사용, 없으면 3000 포트 사용)
 const port = process.env.PORT || 3000;
+
+// cors 설정: 모든 요청에 대해 CORS 허용
+app.use(cors());
 
 // 정적 파일 제공 설정 (public 폴더에서 파일을 제공)
 app.use(express.static(path.join(__dirname, 'public')));
